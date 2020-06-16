9d9feb08d9d552ff5072292be8bb679183d28925
satoyuki <yuki01377@gmail.com>
2020-06-16 21:45:36 +0900

    state up

--- src/main.js
+++ src/main.js
@@ -4,11 +4,29 @@ import ReactDOM from 'react-dom';
 import { SubComponent } from './sub-component';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  handleClick() {
+    console.log('クリックされました');
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Hello React</h1>
-        <SubComponent name="My Counter for Babel"/>
+        <SubComponent name="My Counter for Babel"
+          value={this.state.count}
+          onClick={() => this.handleClick.bind(this)}
+        />
       </div>
     )
   }
